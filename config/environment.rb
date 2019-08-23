acc39c2b88b65854609aa2499e8b18c20c6346ce
charley <gouniafnie@gmail.com>
2019-08-23 11:55:08 +0200

    ajout d'un mailer de test pour heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
 
+ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => "app143906978@heroku.com",
+    :password       => "ecykypad2217",
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
